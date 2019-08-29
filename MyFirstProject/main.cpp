b52a00d916a10875cf30425929d5d4edcb7b1397
Tait Whitten <whitten1@tritons.iowacentral.edu>
2019-08-29 13:09:14 -0600

    Main

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,15 @@
+/*
+	Tait Whitten
+	C++ Fall 2019
+	Due: 29 August 2019
+	Lab 1: Hello World Project
+	This project outputs the words Hello World! and my favorite color
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World! My favorite color is orange";
+	return 0;
+}
